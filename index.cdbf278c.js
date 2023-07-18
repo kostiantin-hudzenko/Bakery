558acb15c8c043c6531c21149137022bcf48a755
Kostiantyn Hudzenko <119158387+kostiantin-hudzenko@users.noreply.github.com>
2023-07-18 10:29:21 +0300

    Add files via upload

---
+++ index.cdbf278c.js
@@ -0,0 +1,2 @@
+"use strict";document.querySelector("form"),window.addEventListener("hashchange",()=>{"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")});
+//# sourceMappingURL=index.cdbf278c.js.map
